7f082537ff5d29fbadfbc822000feb5e2f796843
Chukwuemeka-Ike <chukwuemeka.o.ike@gmail.com>
2020-12-27 00:47:31 -0500

    SL ELM Working

--- elm.py
+++ elm.py
@@ -1,7 +1,5 @@
 import numpy as np
 import csv
-import random
-# import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -9,75 +7,129 @@ import pandas as pd
 filename = 'Datasets/Iris/iris.data'
 trainRatio = 0.8
 
-
+# Loads the data given a filename
 def loadData(filename):
+    # Create the dataframe then convert it to a
+    # numpy array
     data = pd.read_csv(filename, header=None)
-    X = data.to_numpy(dtype=None, copy=False)
+    data = data.to_numpy(dtype=None, copy=False)
     
-    return X
+    return data
 
+# Splits the data according to the given trainRatio
 def splitData(data, trainRatio):
     # Find which classes are which and separate them
-    notSpam = np.array(data[np.where(data[:, -1] == 0)]) 
-    spam = np.array(data[np.where(data[:, -1] == 1)])
+    setosa = data[np.where(data[:, -1] == 'Iris-setosa')]
+    setosa[:,-1] = 1
+    versicolor = data[np.where(data[:, -1] == 'Iris-versicolor')]
+    versicolor[:,-1] = 2
+    virginica = data[np.where(data[:, -1] == 'Iris-virginica')]
+    virginica[:,-1] = 3
 
     # Shuffle the classed data
-    np.random.shuffle(notSpam)
-    np.random.shuffle(spam)
+    np.random.shuffle(setosa)
+    np.random.shuffle(versicolor)
+    np.random.shuffle(virginica)
 
-    # Take the first trainRatio data
-    trainIndex0 = round(trainRatio*len(notSpam))
-    trainData = notSpam[0:trainIndex0, :]
-    testData = notSpam[trainIndex0:len(notSpam), :]
+    # Take the first trainNum of each class
+    trainNum = int(trainRatio*setosa.shape[0])
+    trainSet = setosa[:trainNum,:]
+    trainSet = np.concatenate((trainSet, versicolor[:trainNum,:]), axis=0)
+    trainSet = np.concatenate((trainSet, virginica[:trainNum,:]), axis=0)
 
-    # Take the first trainRatio data
-    trainIndex1 = round(trainRatio * len(spam))
-    trainData = np.concatenate((trainData, spam[0:trainIndex1, :]))
-    testData = np.concatenate((testData, spam[trainIndex1:len(spam), :]))
+    # Take the rest as test data
+    testSet = setosa[trainNum:,:]
+    testSet = np.concatenate((testSet, versicolor[trainNum:,:]), axis=0)
+    testSet = np.concatenate((testSet, virginica[trainNum:,:]), axis=0)
 
-    np.random.shuffle(trainData)
-    np.random.shuffle(testData)
+    # Shuffle the train and test sets
+    np.random.shuffle(trainSet)
+    np.random.shuffle(testSet)
 
-    return trainData, testData
+    trainSet = trainSet.astype(np.float32)
+    testSet = testSet.astype(np.float32)
 
-def takePercentTrain(trainData, percentage):
-    # Get percentage in float
-    percentage /= 100.
+    return trainSet, testSet
 
-    # Find which classes are which and separate them
-    notSpam = np.array(trainData[np.where(trainData[:, -1] == 0)])
-    spam = np.array(trainData[np.where(trainData[:, -1] == 1)])
+# Activation function
+def activate(z, activation):
+    if activation == 'sigmoid':
+        return 1/(1+np.exp(-z))
+    elif activation == 'relu':
+        return np.maximum(0.0,z)
 
-    # Shuffle the classed data
-    np.random.shuffle(notSpam)
-    np.random.shuffle(spam)
+# Compute the accuracy of predictions
+def accuracy(YPred, YTrue):
+    sumAcc = 0
+    if(YPred.size == YTrue.size):
+        for i in np.arange(YTrue.size):
+            if(YPred[i] == YTrue[i]):
+                sumAcc += 1
+    return (sumAcc/YTrue.size) * 100
 
-    # Take the first percentage data
-    trainIndex0 = round(percentage * len(notSpam))
-    trainSplitData = notSpam[0:trainIndex0, :]
-
-    # Take the first percentage data
-    trainIndex1 = round(percentage * len(spam))
-    trainSplitData = np.concatenate((trainSplitData, spam[0:trainIndex1, :]))
-
-    # Shuffle again
-    np.random.shuffle(trainSplitData)
-
-    return trainSplitData
-
-
+# MAIN FUNCTION
 def main():
-    # Load the data into features and labels
-    X = loadData(filename)
-    # print(X[np.where(X[:, -1] == 'Iris-versicolor')][:,-1])
-    # X[np.where(X[:, -1] == 'Iris-setosa')][:,-1] = 2
-    # X[np.where(X[:, -1] == 'Iris-virginica')][:,-1] = 3
-    np.put(X, [np.where(X[:, -1] == 'Iris-versicolor')], 1)
-    print(X[np.where(X[:, -1] == 'Iris-versicolor')])
-    # 'Iris-setosa'
-    # 'Iris-virginica'
-    
-    
+    # Load the data
+    data = loadData(filename)
+
+    # Shuffle and split the data
+    XTrain, XTest = splitData(data, trainRatio)
+    print(XTrain.shape)
+    print(XTest.shape)
+
+    # Separate the data into features and their labels
+    YTrain = XTrain[:,-1]
+    XTrain = XTrain[:,:-1]
+    YTest = XTest[:,-1]
+    XTest = XTest[:,:-1]
+
+    # Now the data is prepped, we can train and 
+    # test the single-layer ELM
+    nHidden = 2
+    activation = 'relu'
+    np.random.seed(0)
+    inputWeights = np.random.rand(XTrain.shape[1], nHidden)
+    inputBias = np.random.rand(nHidden, 1)
+
+    # Compute the forward calculation
+    z = np.matmul(inputWeights.T, XTrain.T) + inputBias
+    z = z.T
+
+    # Activate the computation
+    H = activate(z, activation)
+    H.astype(np.double)
+    print("H Shape:", H.shape)
+
+    # Take the pseudoinverse of H and multiply
+    # it by the labels
+    Beta = np.matmul(np.linalg.pinv(H), YTrain)
+    print("Beta Shape:", Beta.shape)
+
+    # With this Beta, we should be able to carry out 
+    # the classification task on the test data
+    z = np.dot(inputWeights.T, XTest.T) + inputBias
+    z = z.T
+    H = activate(z, activation)
+    print(H.shape)
+
+    YPred = np.matmul(H, Beta)
+
+    # For multi-class classification, it is important that 
+    # we threshold the data in some manner to select
+    # its predicted label
+    for i in range(1,4):
+        pos = np.argwhere(np.abs(YPred-i) < 0.5)
+        for j in pos:
+            YPred[j] = i
+    pos = np.argwhere(YPred < 1)
+    for j in pos:
+            YPred[j] = 1
+    pos = np.argwhere(YPred > 3)
+    for j in pos:
+            YPred[j] = 3
+    print("Predictions Shape:", YPred.shape)
+    acc = accuracy(YPred, YTest)
+    print(acc)
 
 if __name__ == '__main__':
     main()
--- tester.py
+++ tester.py
@@ -1,15 +1,21 @@
 import numpy as np
 
-las = [[1,2,3,4],
+las = [[-1,2,-3,-4],
        [5,6,7,8],
-       [9,1,5,2],
+       [9,1,-5,2],
        [6,2,1,8]
 ]
 
 las = np.array(las)
 # np.put(las, [0, 3, 7], 40)
-np.put(las, np.where(las == 2), 40)
-print(np.where(las == 2))
+
 
 # np.random.shuffle(las)
-print(las)
+print(np.maximum(0,las))
+a = np.greater(las, 0, dtype=np.float32)
+
+yPred = [1.2, 1.4, 2, 0.9, 4, 0.0, -1., 2.3]
+yPred = np.array(yPred)
+pos = np.argwhere(np.abs(yPred-1) < 0.5)
+a = 2.2
+print(f'P: {a}')
